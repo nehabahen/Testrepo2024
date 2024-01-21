7fb26c18b17f7c62a35edd220f6d9bd3b095975d
nehabahen <80130219+nehabahen@users.noreply.github.com>
2024-01-20 22:21:03 -0500

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 20; i++) {
+        // Skip even numbers
+        if (i % 2 == 0 || i% 3 == 0) {    // edited by me(neha) for testing || i% 3 == 0
+            continue;
+        }
+
+       if (i >= 7){  // edited by me(neha) for testing  if satement 
+        break;
+       }
+        // Print odd numbers
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
+
